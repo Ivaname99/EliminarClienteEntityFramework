341caa57509fc14cfec364a49fa93773529873b5
IvElGrimm <126293620+Ivaname99@users.noreply.github.com>
2024-10-12 16:53:32 -0600

    Agregar archivos de proyecto.

---
+++ EliminarEntity/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace EliminarEntity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxID = new System.Windows.Forms.TextBox();
+            this.ID = new System.Windows.Forms.Label();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.dgvProductos = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbxID
+            // 
+            this.tbxID.Location = new System.Drawing.Point(336, 332);
+            this.tbxID.Name = "tbxID";
+            this.tbxID.Size = new System.Drawing.Size(132, 20);
+            this.tbxID.TabIndex = 9;
+            // 
+            // ID
+            // 
+            this.ID.AccessibleRole = System.Windows.Forms.AccessibleRole.ToolTip;
+            this.ID.AutoSize = true;
+            this.ID.Location = new System.Drawing.Point(273, 335);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(57, 13);
+            this.ID.TabIndex = 8;
+            this.ID.Text = "Buscar ID:";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(499, 329);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 7;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(123, 329);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(96, 23);
+            this.btnCargar.TabIndex = 6;
+            this.btnCargar.Text = "Cargar todos";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // dgvProductos
+            // 
+            this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductos.Location = new System.Drawing.Point(34, 103);
+            this.dgvProductos.Name = "dgvProductos";
+            this.dgvProductos.Size = new System.Drawing.Size(701, 200);
+            this.dgvProductos.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AccessibleRole = System.Windows.Forms.AccessibleRole.ToolTip;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(231, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(309, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Eliminar cliente con Entity Framework";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 416);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxID);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.dgvProductos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxID;
+        private System.Windows.Forms.Label ID;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.DataGridView dgvProductos;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
